4759b2099379f571cdc17c6b0bb7a121f6f0ebf2
Kateryna Morhun <kmorhun@gmail.com>
2024-11-03 22:57:53 +0000

    explored some cv inputs

--- queries/rbc_inputs.sql
+++ queries/rbc_inputs.sql
@@ -1,6 +1,9 @@
 SELECT inputs.subject_id, 
     inputs.hadm_id, 
     inputs.icustay_id, 
+    inputs.starttime, 
+    inputs.endtime, 
+    DATETIME_DIFF(inputs.endtime, inputs.starttime, MINUTE) AS duration_minutes,
     inputs.itemid, 
     inputs.rate,
     inputs.rateuom,
@@ -13,24 +16,35 @@ SELECT inputs.subject_id,
         WHEN inputs.amountuom = "uL" THEN "ml"
         ELSE inputs.amountuom 
     END AS amountuom,
+    inputs.totalamount,
+    inputs.totalamountuom,
+    inputs.statusdescription, 
+    inputs.cancelreason, 
     inputs.orderid, 
     inputs.linkorderid
-FROM physionet-data.mimiciii_clinical.inputevents_cv inputs 
+FROM physionet-data.mimiciii_clinical.inputevents_mv inputs 
 INNER JOIN physionet-data.mimiciii_clinical.d_items items ON inputs.itemid = items.itemid
 WHERE inputs.itemid IN 
     (
-        30179,  -- PRBC's
-        30001,  -- Packed RBC's
-        30004   -- Washed PRBC's
+        225168,   -- Packed Red Blood Cells
+        226368,   -- OR Packed RBC Intake
+        227070   -- PACU Packed RBC Intake
     )
 GROUP BY inputs.subject_id, 
     inputs.hadm_id, 
     inputs.icustay_id, 
+    inputs.starttime, 
+    inputs.endtime, 
+    duration_minutes,
     inputs.itemid, 
     inputs.rate,
     inputs.rateuom,
     items.label,
     inputs.amount,
     inputs.amountuom,
+    inputs.totalamount,
+    inputs.totalamountuom,
+    inputs.statusdescription, 
+    inputs.cancelreason, 
     inputs.orderid, 
     inputs.linkorderid;
